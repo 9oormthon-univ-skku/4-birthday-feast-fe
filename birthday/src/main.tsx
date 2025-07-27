39d0761dbfb9d04cb371fb260517734adc46fb8c
twjin03 <twjindev@gmail.com>
2025-07-28 03:52:40 +0900

    feat: 로딩페이지1

--- birthday/src/main.tsx
+++ birthday/src/main.tsx
@@ -3,11 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
 
+<div className="bg-red-300 p-4 text-white text-xl">Tailwind 작동함?</div>
--- birthday/src/pages/Loading.tsx
+++ birthday/src/pages/Loading.tsx
@@ -1,31 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const LoadingWrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-color: #ff8888; // 연한 빨간색 계열
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.h1`
-  font-size: 2.5rem;
-  color: #fff1c1; // 연노랑색
-  font-weight: bold;
-  line-height: 1.5;
-  text-align: center;
-`;
 
 const Loading = () => {
   return (
-    <LoadingWrapper>
-      <Title>
-        생일<br />
-        한상
-      </Title>
-    </LoadingWrapper>
+    <div className="w-screen h-screen relative overflow-hidden">
+
+      {/* 중앙 텍스트 */}
+      <h1
+        className="absolute left-1/2 -translate-x-1/2 
+                  text-[128px] leading-none text-center font-normal text-[#FFEDC8]"
+        style={{
+          top: '28.7%',
+          fontFamily: 'KoreanSWGIG3R, Pretendard, sans-serif',
+        }}
+      >
+        생일<br />한상
+      </h1>
+    </div>
   );
 };
 
