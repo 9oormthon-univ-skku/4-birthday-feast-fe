510ed7aea95abf1a927c725150425a6cdcbbfcc3
twjin03 <twjindev@gmail.com>
2025-10-02 16:55:04 +0900

    fix: 스타일 임포트 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.css';// src/main.tsx
+import './styles/global.css';
+
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
