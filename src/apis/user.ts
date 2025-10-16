5cb284ecf390e9aa651360a5ef3d807136df68f5
twjin03 <twjindev@gmail.com>
2025-10-16 19:31:08 +0900

    feat: 계정 설정 관련 api 엔드포인트 구현

---
+++ src/apis/user.ts
@@ -0,0 +1,26 @@
+import { apiClient } from "@/apis";
+
+// ---------- 타입 정의 ----------
+export type UserMeResponse = {
+  name: string;
+  birthday: string; // YYYY-MM-DD
+  profileImageUrl: string;
+};
+
+// ---------- API 함수 ----------
+/**
+ * [GET] /api-user/me 로그인한 사용자 정보 조회 API
+ * @returns UserMeResponse
+ */
+export async function getUserMe(): Promise<UserMeResponse> {
+  const res = await apiClient.get<UserMeResponse>("/api-user/me");
+  return res.data;
+}
+
+/**
+ * [PATCH] /api-user/me/nickname 닉네임 변경 API
+ * @param nickname 변경할 닉네임
+ */
+export async function updateNickname(nickname: string): Promise<void> {
+  await apiClient.patch("/api-user/me/nickname", { nickname });
+}
