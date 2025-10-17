559f7151f395965b579ace994fe1118dcf7a7a81
twjin03 <twjindev@gmail.com>
2025-10-18 00:17:52 +0900

    refactor: 공용 queryclientprovider 적용 및 쿼리 키 정의

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
-import './index.css';// src/main.tsx
+import './index.css';
 import './styles/global.css';
 
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60, // 1분
+      gcTime: 1000 * 60 * 10, // 10분
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   // <BrowserRouter>
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
   // </BrowserRouter>
   // </React.StrictMode>
 
